64c0f50b79f38b28d4ddf40e07234084198f156a
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-02-06 13:57:33 +0600

    SQL

---
+++ Easy/Advanced/Draw The Triangle 1.sql
@@ -0,0 +1,6 @@
+-- P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+SET @i = 20 + 1;
+
+SELECT REPEAT('* ', @i := @i -1) 
+FROM INFORMATION_SCHEMA.TABLES;
